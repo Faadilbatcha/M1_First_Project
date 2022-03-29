109d422536c0081f591a22255f1e706a3d88caa1
Faadil Batcha <101641134+Faadilbatcha@users.noreply.github.com>
2022-03-29 22:06:48 +0530

    Create version1.h
    Created Version1.h

---
+++ 3_Implementation/inc/version1.h
@@ -0,0 +1,34 @@
+/**
+ * @file  version1.h
+ * @author Faadil Batcha
+ * @version 0.1
+ * @date 2022-03-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __VERSION1_H__
+#define __VERSION1_H__
+
+/**
+ * Include files
+ */ 
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Function declarations
+ */
+void temperature();
+void moisture();
+void ir();
+void gsm();
+void bluetooth();
+void humidity();
+void gas();
+void motion();
+void pulse();
+void sound();
+void interface();
+
+#endif /** __VERSION1_H__ */
